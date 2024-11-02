46f5c83b9bbddbbf3070cf0645353f4c5bbccf1c
fadhil30 <fgeminni0@gmail.com>
2024-11-02 23:54:17 +0700

    first init

---
+++ script.js
@@ -0,0 +1,5 @@
+function showBirthdayWish() {
+  const message =
+    "Happy Birthday, Purwadhika! 🎉 Here’s to another year of inspiring growth and innovation. Wishing you continued success, new milestones, and endless possibilities ahead! 🥂🌟";
+  document.getElementById("wish-message").innerText = message;
+}
