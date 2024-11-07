4eba2eecd392cc89fad8c9696b62f9a5b8cf583b
fadhil30 <fgeminni0@gmail.com>
2024-11-07 09:29:40 +0700

    styling wish message

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function showBirthdayWish() {
   const message = document.getElementById("wish-message");
+  const opening = document.getElementById("wish-opening");
   const audio = document.getElementById("birthday-audio");
-  message, (innerHtml = "");
+  message,
+    (innerHtml =
+      "Happy birthday Purwadhika! Wishing you another amazing year of growth, success, and inspiration. May you continue to transform lives and shape futures through education and innovation. Here’s to more milestones and achievements ahead! 🥂🌟");
   message.classList.add("active");
-
+  opening.classList.add("inactive");
   audio.play();
 }
